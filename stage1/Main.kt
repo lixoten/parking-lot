88232fae2221836ca724976e601de1c01c54c496
lixoten <52259786+lixoten@users.noreply.github.com>
2022-07-13 14:15:28 -0700

    Add files via upload

---
+++ stage1/Main.kt
@@ -0,0 +1,8 @@
+// Stage 1/5: First steps
+package parking
+
+fun main() {
+    println("White car has parked.")
+    println("Yellow car left the parking lot.")
+    println("Green car just parked here.")
+}
